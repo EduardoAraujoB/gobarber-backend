93ac2b5a3f00f8c80b9c11d297aeb131cc09b65b
unknown <ea0123450@gmail.com>
2019-09-03 14:40:43 -0300

    now appointments can be canceled

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,7 @@ import {
   parseISO,
   isBefore,
   format,
+  subHours,
 } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import User from '../models/User';
@@ -133,6 +134,30 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours of advance',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    appointment.save();
 
     return res.json(appointment);
   }
